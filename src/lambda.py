6f99360acf1b84827327d0b43f1c94be0c75c1b0
Rock <soumenrock@users.noreply.github.com>
2025-01-27 01:38:21 +0530

    Update lambda.py

--- src/lambda.py
+++ src/lambda.py
@@ -10,7 +10,7 @@ TABLE_NAME = os.getenv('TABLE')
 
 
 def handler(event, context):
-
+    print(event)
     if event.get('Records'):
         event_time = event['Records'][0]['eventTime']
         s3_details = event['Records'][0]['s3']
@@ -57,8 +57,25 @@ def handler(event, context):
                 }
             return final_response
 
+        elif event['resource'] == '/delete':
+            name = event['headers']['name']
+            file_type = event['headers'].get('type')
+            response = s3_client.delete_object(
+                Bucket=BUCKET_NAME,
+                Key=f'{name}.{file_type}'
+            )
 
+            delete_item = {'name': {'S': name},
+                          'extension': {'S': file_type}}
 
+            response = dynamodb.delete_item(
+                TableName=TABLE_NAME,
+                Key=delete_item
+            )
 
-
+            final_response = {
+                    'statusCode': 202,
+                    'body': "Deleted"
+                }
+            return final_response
 
